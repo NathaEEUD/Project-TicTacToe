91bfad18eb219b68c02255f8805dd8f8df985902
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-02-21 11:17:36 -0500

    feat: Configuring the static files path for the server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
 // settings
 app.set('port', process.env.PORT || 3001);
+app.use(express.static(path.join(__dirname, 'public')));
 
 // start the server
 const server = app.listen(app.get('port'), () => {
