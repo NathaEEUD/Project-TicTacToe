d96a501c9db7ac868c895eefe7da6327463a1fc3
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-02-21 17:47:02 -0500

    feat: Listening the game start event

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -1,1 +1,31 @@
 const socket = io();
+
+let symbol,
+    myTurn;
+
+// functions
+function renderTurnMessage() {
+  $('#leftSide .player-symbol').text(`${symbol}`);
+
+  if (myTurn) {
+    $('#centerSide .alert').text(`Your Turn`);
+    $('.gameholder button').removeAttr('disabled');
+  } else {
+    $('#centerSide .alert').text(`Opponent\'s Turn`);
+    $('.gameholder button').attr('disabled', true);
+  }
+}
+
+// events
+socket.on('server-game.start', (data) => {
+  $('.gameholder button').text('');
+  symbol = data.symbol;
+  myTurn = (symbol === 'X');
+
+  renderTurnMessage();
+});
+
+$(function () {
+  $('.gameholder button').text('');
+  $('.gameholder button').attr('disabled', true);
+})
