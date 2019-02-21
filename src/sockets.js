3044a22010c8dae5bc79894ff5e025121e3d3e66
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-02-21 16:07:36 -0500

    feat: Making the matching of the two connected players

--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,40 @@
 module.exports = function (io) {
 
+  let players = {},
+      unmatched;
+
+  //functions
+  function joinGame(socket) {
+    // save the new user, the first user is going to be X 
+    players[socket.id] = {
+      opponent: unmatched,
+      symbol: 'X',
+      socket
+    };
+
+    if (unmatched) {
+      // when there is a second player, then there is a match
+      players[socket.id].symbol = 'O';
+      players[unmatched].opponent = socket.id;
+      unmatched = null;
+    } else {
+      // when he is the first user, he will be unmatched
+      unmatched = socket.id;
+    }
+
+    console.info(players);
+    console.info(players[socket.id].symbol);
+    console.info(unmatched);
+  }
+
+  function newConnection(socket) {
+    console.log('New connection', socket.id);
+    joinGame(socket);
+  }
+
   // events
   io.on('connection', (socket) => {
-    console.log('New connection', socket.id);
+    newConnection(socket);
   });
 
 }
